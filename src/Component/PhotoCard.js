e3a5d124b316110ec49c67e27b2b0834903eb125
Desi Curry <Blackice@Desis-MacBook-Air.local>
2020-02-06 20:43:23 -0500

    added styles using styled components. MVP Complete.

--- src/Component/PhotoCard.js
+++ src/Component/PhotoCard.js
@@ -1,18 +1,39 @@
 import React from "react"
+import styled from "styled-components";
 
 
-const PhotoCard = props => {
+const DivStyled = styled.div`
+  background-color:rgb(240, 226, 243);
+  margin:10%;
+  img {
+    width: 86%;
+     box-shadow: -7px -10px 50px 16px rgba(59,14,59,0.87);
+  }
+  h2{
+    margin:0 auto;
+    width:86%;
+    text-align:center;
+
+  p {
+      font-size: 30px;
+
+  }  
+  }`
+
+const PhotoCard  = props => {
     return(
-        <div>
+        <DivStyled>
             <h1>{props.title}</h1>
-            <h3>{props.date}</h3>
+            <h2>{props.date}</h2>
             <img src= {props.imgurl} alt="Nasa Astronomy Image"/>
             <div>
-                <h3>Description</h3>
+                <h2>Description</h2>
                 <p>{props.explanation}</p>
             </div>
-        </div>
+        </DivStyled>
+       
     );
 };
+
 
 export default PhotoCard 
